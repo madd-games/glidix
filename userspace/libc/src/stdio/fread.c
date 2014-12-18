b3fd899d082ceca7960df3c4886166d93de97cec
madd-games <madd.gods@gmail.com>
2014-12-18 13:35:05 +0000

    Update fread.c

--- userspace/libc/src/stdio/fread.c
+++ userspace/libc/src/stdio/fread.c
@@ -32,5 +32,13 @@
 size_t fread(void *buf, size_t a, size_t b, FILE *fp)
 {
 	size_t size = a * b;
-	return read(fp->_fd, buf, size) / a;
+	ssize_t ret = read(fp->_fd, buf, size);
+	if (ret >= 0)
+	{
+		return ret / a;
+	}
+	else
+	{
+		return 0;
+	};
 };
