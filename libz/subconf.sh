990ca8d6c4da158f53e992c8ccf67e5900a968fa
Mariusz Pilipczuk <projekt.darma@gmail.com>
2016-12-08 14:57:50 +0000

    Fixed libz problem

--- libz/subconf.sh
+++ libz/subconf.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 srcdir="`dirname $0`"
-$srcdir/configure --prefix=/usr --host=x86_64-glidix
+
+echo >Makefile "SRCDIR := $srcdir"
+echo >>Makefile ".PHONY: all install"
+echo >>Makefile "all: configure"
+echo >>Makefile "	cd build && make"
+echo >>Makefile "install:"
+echo >>Makefile "	cd build && make install"
+echo >>Makefile "configure:"
+echo >>Makefile "	@mkdir -p build"
+echo >>Makefile "	cd build && ../\$(SRCDIR)/configure --prefix=/usr --host=x86_64-glidix --disable-static"
+echo >>Makefile "	touch configure"
