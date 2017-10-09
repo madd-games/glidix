e8422a40e3ead1cdbbad4b19e180c812dce44ade
Doomjoon <damjan.majic@gmail.com>
2017-10-09 21:52:56 +0200

    Audio stuff initialized

---
+++ kernel/include/glidix/audio.h
@@ -0,0 +1,90 @@
+/*
+	Glidix kernel
+
+	Copyright (c) 2014-2017, Madd Games.
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+	
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef __glidix_audio_h
+#define __glidix_audio_h
+
+#include <glidix/common.h>
+
+/**
+ * Audio stream.
+ */
+typedef struct 
+{
+	/**
+	 * Sample rate (bitrate) of the stream.
+	 */
+	int sampleRate;
+	
+	/**
+	 * Number of bits per sample.
+	 */
+	int bitsPSample;
+	
+	/**
+	 * Number of samples in a buffer.
+	 */
+	int nOfSamples;
+	
+	/**
+	 * Number of audio channels.
+	 */
+	int nOfChannels;
+	
+	/**
+	 * Head of the buffer.
+	 */
+	off_t head;
+	
+	/**
+	 * Pointer to the buffer.
+	 */
+	void *buffer;
+} AudioStream;
+
+/**
+ * Member of application buffer list for audio.
+ */
+typedef struct AudioAppBuffer_
+{
+	struct AudioAppBuffer_ *next;
+	struct AudioAppBuffer_ *prev;
+	struct AudioStream_ *audioStream;
+} AudioAppBuffer;
+
+/**
+ * Member of audio output list.
+ */
+typedef struct AudioOutput_
+{
+	struct AudioOutput_ *next;
+	struct AudioOutput_ *prev;
+	struct AudioStream_ *audioStream;
+} AudioOutput;
+
+#endif
