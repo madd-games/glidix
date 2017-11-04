5052c8305a07c23c925d1f447cbf45cf3ea4eb42
Mariusz Pilipczuk <projekt.darma@gmail.com>
2017-11-04 20:33:18 +0000

    vfs.h stuff

--- kernel/include/glidix/vfs.h
+++ kernel/include/glidix/vfs.h
@@ -50,43 +50,12 @@
 #define	VFS_MODE_STICKY			01000
 
 #define	VFS_MODE_REGULAR		0		/* 0, so default */
-#define	VFS_MODE_DIRECTORY		010000
-#define	VFS_MODE_CHARDEV		020000
-#define	VFS_MODE_BLKDEV			030000
-#define	VFS_MODE_FIFO			040000
-#define	VFS_MODE_LINK			050000		/* soft link */
-#define	VFS_MODE_SOCKET			060000
-
-/**
- * Errors.
- */
-#define	VFS_EMPTY_DIRECTORY		-1		/* the directory was empty */
-#define	VFS_BAD_FD			-2		/* bad file descriptor */
-#define	VFS_PERM			-3		/* permission denied */
-#define	VFS_NO_FILE			-4
-#define	VFS_FILE_LIMIT_EXCEEDED		-5		/* MAX_OPEN_FILES limit exceeded */
-#define	VFS_BUSY			-6		/* a file is busy */
-#define	VFS_NOT_DIR			-7		/* not a directory */
-#define	VFS_LINK_LOOP			-8		/* symbolic link depth too high */
-#define	VFS_IO_ERROR			-9		/* other, unknown problem (e.g. readlink failed) */
-#define	VFS_NO_MEMORY			-10		/* not enough memory */
-#define	VFS_NO_SPACE			-11		/* insufficient storage space */
-#define	VFS_READ_ONLY			-12		/* read-only filesystem */
-#define	VFS_EXISTS			-13		/* name already exists */
-#define	VFS_LONGNAME			-14		/* name too long */
-
-/**
- * Flags to parsePath()/vfsOpen().
- */
-#define	VFS_CHECK_ACCESS		(1 << 0)
-// if set, and parsePath() finds an empty directory while searching, it will return the end directory pointer
-// for it, and set *error to VFS_EMPTY_DIRECTORY.
-#define	VFS_STOP_ON_EMPTY		(1 << 1)
-// if set, parsePath() will create a regular file if it does not exist.
-#define	VFS_CREATE			(1 << 2)
-/* next 12 bits reserved */
-// if final component is a symlink, do not follow
-#define	VFS_NO_FOLLOW			(1 << 14)
+#define	VFS_MODE_DIRECTORY		0x1000
+#define	VFS_MODE_CHARDEV		0x2000
+#define	VFS_MODE_BLKDEV			0x3000
+#define	VFS_MODE_FIFO			0x4000
+#define	VFS_MODE_LINK			0x5000		/* soft link */
+#define	VFS_MODE_SOCKET			0x6000
 
 /**
  * Flags for the open() system call.
@@ -115,8 +84,7 @@
 #define	O_TERMINAL			(1 << 18)
 
 /**
- * File descriptor flags settable with fcntl(). They share space with the O_* flags because
- * they are stored in the same field. Some match up with the O_* flags.
+ * File descriptor flags settable with fcntl().
  */
 #define	FD_CLOEXEC			O_CLOEXEC
 #define	FD_ALL				(FD_CLOEXEC)
@@ -142,13 +110,12 @@
 #define	POLL_HANGUP			(1 << PEI_HANGUP)
 #define	POLL_INVALID			(1 << PEI_INVALID)
 
-#define	FILENAME_MAX			128
-#define	PATH_MAX			256
-#define	VFS_MAX_LINK_DEPTH		8
-#define	VFS_MAX_LOCKS_PER_FILE		32
+/**
+ * Inode flags.
+ */
+#define	VFS_INODE_DIRTY			(1 << 0)		/* inode changed since loading */
 
-#define	VFS_NAME_MAX			128
-#define	VFS_PATH_MAX			256
+#define	VFS_MAX_LINK_DEPTH		8
 
 #define	VFS_ACL_SIZE			128
 
@@ -230,317 +197,100 @@ typedef struct SysObject_
 } SysObject;
 
 /**
- * Describes an open file. All of its member functions may be NULL.
+ * All the structure typedefs here, definitions below.
  */
-typedef struct _File
+typedef struct Inode_ Inode;
+typedef struct Dentry_ Dentry;
+typedef struct File_ File;
+typedef struct FileSystem_ FileSystem;
+
+/**
+ * Describes a VFS inode.
+ */
+struct Inode_
 {
 	/**
-	 * Private, filesystem-specific structure.
-	 */
-	void *fsdata;
-
-	/**
-	 * oflag used to open the file.
-	 */
-	int oflag;
-
-	/**
-	 * File reference count. The file is closed and the memory is released once this reaches
-	 * zero. Use vfsDup() and vfsClose() to manage this.
-	 *
-	 * For files opened by the kernel, this is allowed to be zero. In this case, you must have
-	 * only one reference, and it may be destroyed with vfsClose().
+	 * Reference count. This is the number of references to this inode which exist
+	 * in memory (not necessarily on disk; "links" is for that).
 	 */
 	int refcount;
 	
 	/**
-	 * Read the file.
+	 * Specifies which filesystem this inode belongs to. This may be NULL if the
+	 * inode is not on any filesystem (e.g. a socket or a pipe).
 	 */
-	ssize_t (*read)(struct _File *file, void *buffer, size_t size);
-
-	/**
-	 * Write to the file.
-	 */
-	ssize_t (*write)(struct _File *file, const void *buffer, size_t size);
-
-	/**
-	 * Seek to a different position in the file. Return the old position.
-	 */
-	off_t (*seek)(struct _File *file, off_t offset, int whence);
-
-	/**
-	 * Close the file. Do not free this structure! The kernel will do that for you.
-	 * This is called when refcounts go to zero.
-	 */
-	void (*close)(struct _File *file);
-
-	/* we're gonna remove this soon when we revamp the VFS */
-	void *resv;
-
-	/**
-	 * This is used to implement the ioctl() interface. The 'cmd' argument must be constructed
-	 * according to the rules from <glidix/ioctl.h>. The last argument is undefined for 0-sized
-	 * commands. Return 0 on success, -1 on error. All ioctl() calls will fail with EINVAL if
-	 * this entry is NULL.
-	 */
-	int (*ioctl)(struct _File *file, uint64_t cmd, void *argp);
-
-	/**
-	 * Get the file state. Returns 0 on success, -1 on failure. If this is NULL then fstat() on
-	 * this file description always fails, which is a bad thing. If -1 is returned or this function
-	 * pointer is NULL, then EIO is returned to the caller.
-	 */
-	int (*fstat)(struct _File *file, struct kstat *st);
-
-	/**
-	 * Like chmod() in DIR, except takes a file description instead of a directory pointer.
-	 */
-	int (*fchmod)(struct _File *file, mode_t mode);
-
-	/**
-	 * Like chown() in DIR, except takes a file description instead of a directory pointer.
-	 */
-	int (*fchown)(struct _File *file, uid_t uid, gid_t gid);
-
-	/**
-	 * Synchronise file contents with the disk. This function shall wait until all data modified in the
-	 * file has been written to disk. It cannot fail.
-	 */
-	void (*fsync)(struct _File *file);
-
-	/**
-	 * Truncate the file to the specified size. If the size is actually larger than the current size, then
-	 * zero-pad the file until it is the specified size. This can never fail if defined.
-	 */
-	void (*truncate)(struct _File *file, off_t length);
-
-	/**
-	 * Returns the FileTree representing this file, if it is random-access. The reference count of the tree
-	 * shall be incremented, and it is the caller's responsibility to decrement it later.
-	 */
-	FileTree* (*tree)(struct _File *file);
+	FileSystem* fs;
 	
 	/**
-	 * Reading/writing at arbitrary offsets, thread-safe calls for random-access files. Most importantly,
-	 * this is used by memory-mapped files.
+	 * The inode number on the filesystem.
 	 */
-	ssize_t (*pread)(struct _File *file, void *buf, size_t count, off_t offset);
-	ssize_t (*pwrite)(struct _File *file, const void *buffer, size_t count, off_t offset);
+	ino_t ino;
 	
 	/**
-	 * Get pollable event information about this file. 'sems' points to an array of pointers to 8 semaphores,
-	 * each index by an PEI_* macro. An event is said to occur when its semaphore value becomes nonzero.
-	 * For example, if the semaphore indexed by PEI_READ has a nonzero value (i.e. a semWait() on it would
-	 * not block), the file is considered ready for reading. The array is initialized by the system to hold 8
-	 * NULL pointers. A NULL pointer indicates that an event will never occur; so if the PEI_WRITE entry is
-	 * set to NULL, the file is never considered writeable. If you want a file to always be considered, for
-	 * example, readable, you must set PEI_READ (or whatever event is always "occuring") to the return value
-	 * of vfsGetConstSem() - which is a semaphore that always has value 1. If this function is not present,
-	 * then all entries in that array will remain NULL and will never occur. If present, it shall always
-	 * be successful as it should only fill in pointers to static per-file variables.
+	 * Access mode and permissions.
 	 */
-	void (*pollinfo)(struct _File *file, struct Semaphore_ **sems);
-} File;
-
-/**
- * This structure is used to describe an open directory scanner.
- */
-typedef struct _Dir
-{
+	mode_t mode;
+	
 	/**
-	 * Private, filesystem-specific structure.
+	 * Number of links to this inode. This is the number of file descriptions referring
+	 * to it, plus the number of hard links on disk.
+	 */
+	nlink_t links;
+	
+	/**
+	 * Owner and associated group.
+	 */
+	uid_t uid;
+	gid_t gid;
+	
+	/**
+	 * Block size and number of blocks currently taken on disk.
+	 */
+	blksize_t blockSize;
+	blkcnt_t numBlocks;
+	
+	/**
+	 * File times.
+	 */
+	time_t atime, mtime, ctime, btime;
+	
+	/**
+	 * Executable permissions.
+	 */
+	uint64_t ixperm, oxperm, dxperm;
+	
+	/**
+	 * Access control list.
+	 */
+	AccessControlEntry acl[VFS_ACL_SIZE];
+	
+	/**
+	 * Inode flags (VFS_INODE_*).
+	 */
+	int flags;
+	
+	/**
+	 * The file tree, if this is a random-access file (else NULL).
+	 */
+	FileTree* ft;
+	
+	/**
+	 * Driver-specific inode info.
 	 */
 	void *fsdata;
-
-	/**
-	 * Description of the current entry and its stat().
-	 */
-	struct kdirent			dirent;
-	struct kstat			stat;
-	char				_pad[1024 - sizeof(struct kstat) - sizeof(struct kdirent)];
 	
 	/**
-	 * If this entry points to a file, open the file. Return 0 on success, -1 on error.
-	 * The passed File structure was initialized to default values; please only change
-	 * the values that you need to. Also, the size was passed in case there is a mismatch
-	 * between the target kernel version of a module and the actual kernel version.
+	 * If this is a symlink, this is the target path.
 	 */
-	int (*openfile)(struct _Dir *me, File *file, size_t szFile);
-
-	/**
-	 * Same as openfile(), except opens a directory. It may return error codes as described above.
-	 */
-	int (*opendir)(struct _Dir *me, struct _Dir *dir, size_t szDir);
-
-	/**
-	 * Close this scanner. Do not free the Dir structure! The kernel will do that for you.
-	 */
-	void (*close)(struct _Dir *dir);
-
-	/**
-	 * Advance to the next entry in this directory. Return 0 on success, -1 on end of list.
-	 * If this is the last entry, and the filesystem is not read-only, this structure shall
-	 * remain valid so that mkdir() or mkreg() can be called to add a new entry.
-	 */
-	int (*next)(struct _Dir *dir);
-
-	/**
-	 * Change the bottom 12 bits of st_mode, and save that change to disk. Do not perform any security
-	 * checks. If the change cannot be saved, then this function shall return -1; else the function
-	 * shall return 0. Please note that the top 4 bits of mode shall be ignored and the top 4 bits of
-	 * st_mode shall remain unchanged!
-	 */
-	int (*chmod)(struct _Dir *dir, mode_t mode);
-
-	/**
-	 * Change the value of st_uid and st_gid, and save that change to disk. Do not perform any security
-	 * checks. If the change cannot be saved, then this function shall return -1; else the function
-	 * shall return 0.
-	 */
-	int (*chown)(struct _Dir *dir, uid_t uid, gid_t gid);
-
-	/**
-	 * Create a new directory. 'dir' is guaranteed to be a directory end pointer (i.e. the state of a DIR
-	 * structure after a call to next() that returned -1). This function shall add a new directory entry
-	 * to 'dir', which shall represent a directory with name 'name' and access mode 'mode'. It is guaranteed
-	 * that this directory does not already contain an entry with the specified name. It is also guaranteed
-	 * that 2 mkdir() (or other creation or removal) requests do not happen at the same time.
-	 */
-	int (*mkdir)(struct _Dir *dir, const char *name, mode_t mode, uid_t uid, gid_t gid);
-
-	/**
-	 * Create a new regular file. 'dir' is guaranteed to be a directory end pointer. This function shall add
-	 * a new directory entry to 'dir', which shall represent a new file with name 'name', and have the specified
-	 * mode, owner and group. It is guaranteed that this direcotry does not already contain an entry with the
-	 * specified name, and 2 mkreg() (or other creation or removal) requests do not happen at the same time.
-	 * Return -1 on failure, or 0 on success; if successful, 'dir' shall be modified to point to this new file.
-	 */
-	int (*mkreg)(struct _Dir *dir, const char *name, mode_t mode, uid_t uid, gid_t gid);
-
-	/**
-	 * Remove the directory entry pointed to by this directory pointer. If the entry is the only link to the inode
-	 * left, then the inode is also removed, along with all its contents. If the file is currently open, then the
-	 * inode shall be removed after all file desctipions referring to it are closed. It is guaranteed that 2 calls
-	 * to unlink() or any other creation/deletion function do not happen at the same time. Return 0 on success, or
-	 * -1 on error. This function will only be called on empty directories; the kernel will assume that a directory
-	 * is empty if st_size is 0.
-	 */
-	int (*unlink)(struct _Dir *dir);
-
-	/**
-	 * Create a hard link in this directory to an already-existing file. 'dir' is guaranteed to be a directory end pointer.
-	 * It is guranteed that this directory does not already contain an entry with the given name, and 2 link() (or other
-	 * creation or removal) requests do not happen at the same time. Return -1 on error (EIO) or 0 on success.
-	 * 'ino' is guranteed to be the inode number of a file on the same filesystem (i.e. with st_dev matching that of this
-	 * directory).
-	 */
-	int (*link)(struct _Dir *dir, const char *name, ino_t ino);
-
-	/**
-	 * Create a symbolic/soft link with the specified name, and store the specified path. See mkreg() for more info on
-	 * file-creation semantics. Returns 0 on success, or -1 on error (EIO).
-	 */
-	int (*symlink)(struct _Dir *dir, const char *name, const char *path);
-
-	/**
-	 * Read the path of a symbolic link pointed to by this directory pointer. The path shall be stored in 'buffer', which
-	 * has a length of at least PATH_MAX. On failure, return -1; otherwise, return the actual size of the path, which must
-	 * be smaller than PATH_MAX. Also, the path must be NUL-terminated.
-	 */
-	ssize_t (*readlink)(struct _Dir *dir, char *buffer);
-
-	/**
-	 * Used to implement efficient stat(). If this function is NULL, then it is expected that opendir(), openroot() and
-	 * next() will automatically set the 'stat' field to a valid value. If this function IS implemented, then the stat
-	 * field should only be filled in when this is called.
-	 */
-	void (*getstat)(struct _Dir *dir);
-
-	/**
-	 * Change the access and modification times of this file. Return 0 on success, -1 on failure.
-	 */
-	int (*utime)(struct _Dir *dir, time_t atime, time_t mtime);
+	char *target;
 	
 	/**
-	 * Change the executable permissions on this file. If an argument is XP_NCHG, it means that this field shall
-	 * not be set. Return 0 on success, -1 on I/O error.
+	 * Head of the dentry cache, if this is a directory inode.
 	 */
-	int (*chxperm)(struct _Dir *dir, uint64_t ixperm, uint64_t oxperm, uint64_t dxperm);
-	
-	/**
-	 * Link a system object into the specified name in this directory. The owner and group are set according
-	 * to the EUID and EGID. This function increments the reference count of the object. Return 0 on success,
-	 * -1 on error (which then gets reported as EIO). System objects do not persist after reboot or unmount.
-	 */
-	int (*linksys)(struct _Dir *dir, const char *name, SysObject *sysobj);
-	
-	/**
-	 * If the current entry is a system object, increment the reference count and return it. Otherwise, return
-	 * NULL.
-	 */
-	SysObject* (*getsys)(struct _Dir *dir);
-
-	/**
-	 * Set the permissions of a given user (type == VFS_ACE_USER) or group (VFS_ACE_GROUP) in the access control
-	 * list. If an entry for this entity already exists, it is updated, otherwise, it is added (if there is space).
-	 * Return 0 on success or -1 on error (and set ERRNO). ERRNO shall be set to EOVERFLOW if there is no space on
-	 * the list, ENOSPC if the filesystem needs to allocate space on disk for the ACL and it was not possible.
-	 * Other errors may also be returned.
-	 */
-	int (*aclput)(struct _Dir *dir, uint8_t type, uint16_t id, uint8_t perms);
-	
-	/**
-	 * Remove the specified entry from the ACL. Return 0 on success, or -1 on error (and set ERRNO). ERRNO shall be
-	 * set to EINVAL if the entry does not exist.
-	 */
-	int (*aclclear)(struct _Dir *dir, uint8_t type, uint16_t id);
-} Dir;
+	Dentry* dents;
+};
 
 struct fsinfo;
-
-/**
- * Describes a mounted instance of a filesystem, perhaps attached to a storage device.
- */
-typedef struct _FileSystem
-{
-	/**
-	 * Private, filesystem-specific data structure.
-	 */
-	void *fsdata;
-
-	/**
-	 * Open the root directory of this filesystem. Return 0 on success, a negative number
-	 * on error; see above for more info.
-	 */
-	int (*openroot)(struct _FileSystem *fs, Dir *dir, size_t szDir);
-
-	/**
-	 * Unmount this filesystem. Do not free this structure! The kernel does that for you.
-	 * Return 0 on success, -1 on error (reported as EBUSY).
-	 */
-	int (*unmount)(struct _FileSystem *fs);
-
-	/**
-	 * The name of this filesystem.
-	 */
-	const char *fsname;
-	
-	/**
-	 * Device ID, assigned during mount.
-	 */
-	dev_t dev;
-	
-	/**
-	 * The image with which this filesystem was mounted; assigned during mount.
-	 */
-	char imagename[256];
-	
-	/**
-	 * Get filesystem statistics. This must fill in the fs_usedino, fs_inodes, fs_usedblk, fs_blocks,
-	 * and fs_blksize members of the 'info' structure, if known.
-	 */
-	void (*getinfo)(struct _FileSystem *fs, struct fsinfo *info);
-} FileSystem;
 
 /**
  * Filesystem information structure, for _glidix_fsinfo().
