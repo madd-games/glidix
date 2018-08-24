c425e8fdc99379960dbd97fff98e154515fede9b
Mariusz Pilipczuk <projekt.darma@gmail.com>
2018-08-24 23:49:02 +0100

    License now embedded in libgwm.h

--- minipad/src/minipad.c
+++ minipad/src/minipad.c
@@ -121,31 +121,7 @@ void showAbout()
 	gwmSetAboutIcon(about, icon, 16, 0, 24, 24);
 	gwmSetAboutCaption(about, "Minipad");
 	gwmSetAboutDesc(about, "The native text editor of Glidix.");
-	gwmSetAboutLicense(about, "Glidix GUI\n\
-\n\
-Copyright (c) 2014-2017, Madd Games.\n\
-All rights reserved.\n\
-\n\
-Redistribution and use in source and binary forms, with or without\n\
-modification, are permitted provided that the following conditions are met:\n\
-\n\
-* Redistributions of source code must retain the above copyright notice, this\n\
-  list of conditions and the following disclaimer.\n\
-\n\
-* Redistributions in binary form must reproduce the above copyright notice,\n\
-  this list of conditions and the following disclaimer in the documentation\n\
-  and/or other materials provided with the distribution.\n\
-\n\
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n\
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n\
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n\
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\n\
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n\
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\n\
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n\
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\n\
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n\
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n");
+	gwmSetAboutLicense(about, GWM_LICENSE);
 	gwmRunAbout(about);
 };
 
