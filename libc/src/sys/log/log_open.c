3b7040359ffe3202ef465016bbadc46fbd5b05f7
Mariusz Pilipczuk <projekt.darma@gmail.com>
2018-09-17 01:39:02 +0100

    Log functionality in libc

---
+++ libc/src/sys/log/log_open.c
@@ -0,0 +1,60 @@
+/*
+	Glidix Runtime
+
+	Copyright (c) 2014-2017, Madd Games.
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+	
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <sys/log.h>
+#include <unistd.h>
+
+int __log_open(const char *prefix)
+{
+	int pipefd[2];
+	if (pipe(pipefd) != 0)
+	{
+		return -1;
+	};
+	
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		return -1;
+	}
+	else if (pid == 0)
+	{
+		close(pipefd[1]);
+		close(0);
+		dup(pipefd[0]);
+		close(pipefd[0]);
+		
+		execl("/usr/bin/log", "log", prefix, NULL);
+		_exit(1);
+	}
+	else
+	{
+		close(pipefd[0]);
+		return pipefd[1];
+	};
+};
