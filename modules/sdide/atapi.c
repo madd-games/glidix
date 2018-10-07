469883bcdfd36c6e5086acd736640a2e8a3f10a8
Mariusz Pilipczuk <projekt.darma@gmail.com>
2018-10-07 12:20:53 +0100

    ATAPI eject

--- modules/sdide/atapi.c
+++ modules/sdide/atapi.c
@@ -281,9 +281,110 @@ size_t atapiGetSize(void *drvdata)
 	return 0;
 };
 
+int atapiEject(void *drvdata)
+{
+	IDEDevice *dev = (IDEDevice*) drvdata;
+	IDEController *ctrl = dev->ctrl;
+	int channel = dev->channel;
+	
+	semWait(&dev->ctrl->lock);
+	
+	// enable interrupts
+	outb(dev->ctrl->channels[dev->channel].ctrl + ATA_CREG_CONTROL, 0);
+	ideInts[dev->channel] = 0;
+	
+	// ATAPI packet to send
+	uint8_t atapiPacket[12];
+	atapiPacket[0] = ATAPI_CMD_EJECT;
+	atapiPacket[1] = 0;
+	atapiPacket[2] = 0;
+	atapiPacket[3] = 0;
+	atapiPacket[4] = 0x02;
+	atapiPacket[5] = 0;
+	atapiPacket[6] = 0;
+	atapiPacket[7] = 0;
+	atapiPacket[8] = 0;
+	atapiPacket[9] = 0;
+	atapiPacket[10] = 0;
+	atapiPacket[11] = 0;
+	
+	// select drive
+	outb(ctrl->channels[channel].base + ATA_IOREG_HDDEVSEL, dev->slot << 4);
+	int i;
+	for (i=0; i<4; i++)
+	{
+		inb(ctrl->channels[channel].ctrl + ATA_CREG_CONTROL);
+	};
+	
+	// use PIO mode
+	outb(ctrl->channels[channel].base + ATA_IOREG_FEATURES, 0);
+	
+	// send the packet command
+	outb(ctrl->channels[channel].base + ATA_IOREG_COMMAND, ATA_CMD_PACKET);
+	for (i=0; i<4; i++)
+	{
+		inb(ctrl->channels[channel].ctrl + ATA_CREG_CONTROL);
+	};
+	
+	// wait for it to stop being busy
+	while (inb(ctrl->channels[channel].base + ATA_IOREG_STATUS) & ATA_SR_BSY);
+	
+	// check for errors
+	uint8_t status = inb(ctrl->channels[channel].base + ATA_IOREG_STATUS);
+	int error = 0;
+	
+	if (status & ATA_SR_ERR)
+	{
+		error = 1;
+	};
+	
+	if (status & ATA_SR_DF)
+	{
+		error = 1;
+	};
+	
+	if ((status & ATA_SR_DRQ) == 0)
+	{
+		error = 1;
+	};
+				
+	if (!error)
+	{
+		// send the packet over
+		outsw(ctrl->channels[channel].base + ATA_IOREG_DATA, atapiPacket, 6);
+
+		int k;
+		for (k=0; k<4; k++)
+		{
+			inb(ctrl->channels[channel].ctrl + ATA_CREG_CONTROL);
+		};
+		
+		// wait for it to stop being busy
+		while (inb(ctrl->channels[channel].base + ATA_IOREG_STATUS) & ATA_SR_BSY);
+
+		// wait for interrupt
+		while (!ideInts[dev->channel]);
+		ideInts[dev->channel] = 0;
+
+		for (k=0; k<4; k++)
+		{
+			inb(ctrl->channels[channel].ctrl + ATA_CREG_CONTROL);
+		};
+		
+		// wait for it to stop being busy
+		while (inb(ctrl->channels[channel].base + ATA_IOREG_STATUS) & ATA_SR_BSY);
+	};
+
+	semSignal(&dev->ctrl->lock);
+	
+	if (error) return EIO;
+	return 0;
+};
+
 SDOps atapiOps = {
 	.size = sizeof(SDOps),
 	.readBlocks = atapiReadBlocks,
 	.writeBlocks = atapiWriteBlocks,
 	.getSize = atapiGetSize,
+	.eject = atapiEject,
 };
