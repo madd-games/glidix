a94f0b90fd51135046c911aa5b9b0b6af6734031
Mariusz Pilipczuk <projekt.darma@gmail.com>
2023-11-11 14:01:43 +0000

    Generate compile_commands.json

--- dist/do-iso-build.inc.sh
+++ dist/do-iso-build.inc.sh
@@ -3,6 +3,11 @@
 log_info "Building the distro ISO..."
 cd iso || exit 1
 
-make || exit 1
+if command -v bear >/dev/null 2>&1; then
+	log_info '`bear` detected; compile_commands.json will be generated/updated during the build'
+	bear --output ../compile_commands.json --append -- make || exit 1
+else
+	make || exit 1
+fi
 
 log_success "DISTRO BUILD SUCCESSFUL."
