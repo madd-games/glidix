a736320846971583ad30345dbb33ca21131a42d9
Mariusz Pilipczuk <projekt.darma@gmail.com>
2023-11-18 03:10:41 +0000

    Make it re-create the image

--- scripts/make-diskimg.sh
+++ scripts/make-diskimg.sh
@@ -1,4 +1,4 @@
-# Syntax: make-diskimg.sh <imgdir> <output_file>
+# Syntax: make-diskimg.sh <imgdir> <diskimg_file>
 #
 # Creates a bootable disk image with the specified directory being copied as the root
 # filesystem.
@@ -8,15 +8,14 @@ DISKIMG_DEFAULT_SIZE="6G"
 
 # Get arguments.
 imgdir="$1"
-output_file="$2"
+diskimg_file="$2"
 
-# If the disk image does not yet exist, then create it with a default size,
-# and create the partition table.
-if [ ! -e $output_file ]
-then
-	fallocate -l $DISKIMG_DEFAULT_SIZE $output_file || exit 1
-	build-tools/mkgpt $output_file \
-		--part type=efisys size=$DISKIMG_EFISYS_MB name='EFI System Partition' \
-		--part type=gxfs-root name='Glidix Installer' \
-	|| exit 1
-fi
+# Delete the image if it already exists, and create it with the default size.
+rm -f $diskimg_file || exit 1
+fallocate -l $DISKIMG_DEFAULT_SIZE $diskimg_file || exit 1
+
+# Create the partition table on the image.
+build-tools/mkgpt $diskimg_file \
+	--part type=efisys size=$DISKIMG_EFISYS_MB name='EFI System Partition' \
+	--part type=gxfs-root name='Glidix Installer' \
+|| exit 1
